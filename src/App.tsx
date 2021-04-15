51359991f87bdeda33135839c7f86da0d408b634
tayutaedomo <tayutaedomo@gmail.com>
2021-04-15 22:00:00 +0900

    add: getUserMedia API call

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
-function App() {
+const getMedia = async () => {
+  const constraints = { audio: true, video: true };
+
+  try {
+    return await navigator.mediaDevices.getUserMedia(constraints);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+getMedia();
+
+const App = () => {
   return <div>App</div>;
-}
+};
 
 export default App;
