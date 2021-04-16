33895ab51ca9b5928c6dd266fcfe48227a770f92
tayutaedomo <tayutaedomo@gmail.com>
2021-04-16 14:56:41 +0900

    refactor: Add InputForm component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
 
 import useRtcClient from './hooks/useRtcClient';
-import InputFromLocal from './InputFromLocal';
-import InputFromRemote from './InputFromRemote';
+import InputForm from './InputForm';
 import VideoArea from './VideoArea';
 
 const App = () => {
   const rtcClient = useRtcClient();
 
-  if (rtcClient === null) return <></>;
-
   return (
     <>
-      <InputFromLocal rtcClient={rtcClient} />
-      <InputFromRemote rtcClient={rtcClient} />
+      <InputForm rtcClient={rtcClient} />
       <VideoArea rtcClient={rtcClient} />
     </>
   );
